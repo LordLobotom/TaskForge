2a7e4b03cb98fbf18bd0c6656f2c1e5c39421684
Gutka <eMichalGut@gmail.com>
2025-08-14 10:43:22 +0200

    program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.entityFrameworkCore;
+using TaskForge.Data;
+using TaskForge.Services;
 using TaskForge.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +9,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Registrace Entity Framework s SQLite databází
+builder.Services.AddDbContext<TaskForgeDbContext>(options =>
+    options.UseSqlite("Data Source=TaskForge.db"));
+
+// Registrujeme naši službu pro práci s úkoly
+builder.Services.AddScoped<IUkolService, UkolService>();
+
 var app = builder.Build();
+
+// vytvoření databáze a tabulek
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<TaskForgeDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
